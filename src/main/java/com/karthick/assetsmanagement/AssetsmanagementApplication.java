af94f583e38cc628abe2c0d1c857becb09374d2d
Karthick-sketch <karthick246.kh@outlook.com>
2023-03-31 17:59:35 +0530

    first commit

---
+++ src/main/java/com/karthick/assetsmanagement/AssetsmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.karthick.assetsmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetsmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssetsmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/karthick/assetsmanagement/AssetsmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karthick.assetsmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetsmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
